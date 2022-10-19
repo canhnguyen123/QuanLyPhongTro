9a67b9f57ae0f5981625814a1dd37ae5b253f971
daoTuan <minhtuan080722@gmail.com>
2022-10-19 21:42:08 +0700

    nhiu lam

--- app/src/main/java/com/example/quanlyduan/fragment/phongtro_Fragment.java
+++ app/src/main/java/com/example/quanlyduan/fragment/phongtro_Fragment.java
@@ -1,5 +1,8 @@
 package com.example.quanlyduan.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -26,6 +29,22 @@ public class phongtro_Fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fab = view.findViewById(R.id.fabaddphong);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog(getContext());
+            }
+        });
+    }
+
+    private void openDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_addphong, null);
+        builder.setView(view);
+        Dialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
